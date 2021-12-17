dea46e905223449d99fcad32f84afee321543ff5
atractylodis <atractylodis@gmail.com>
2021-12-17 14:50:49 +0800

    new

--- JUC/src/main/java/synchronized_/SynchronizedDemo.java
+++ JUC/src/main/java/synchronized_/SynchronizedDemo.java
@@ -6,16 +6,16 @@ import lombok.extern.slf4j.Slf4j;
 public class SynchronizedDemo {
     public static void main(String[] args) {
         SynchronizedTest obj = new SynchronizedTest();
-        Thread t1=new Thread(() -> {
+        Thread t1 = new Thread(() -> {
             try {
-                obj.method();
+                obj.run();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        Thread t2=new Thread(() -> {
+        Thread t2 = new Thread(() -> {
             try {
-                obj.method();
+                obj.run();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- JUC/src/main/java/synchronized_/SynchronizedTest.java
+++ JUC/src/main/java/synchronized_/SynchronizedTest.java
@@ -4,9 +4,25 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SynchronizedTest {
+    private final Object block1 = new Object();
+    private final Object block2 = new Object();
+
     public synchronized void method() throws InterruptedException {
-        log.info("thread "+Thread.currentThread().getName()+" start...");
+        log.info("thread " + Thread.currentThread().getName() + " start...");
         Thread.sleep(3000);
-        log.info("thread "+Thread.currentThread().getName()+" end...");
+        log.info("thread " + Thread.currentThread().getName() + " end...");
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (block1) {
+            log.info("block1 " + Thread.currentThread().getName() + " start...");
+            Thread.sleep(3000);
+            log.info("block1 " + Thread.currentThread().getName() + " end...");
+        }
+        synchronized (block2) {
+            log.info("block2 " + Thread.currentThread().getName() + " start...");
+            Thread.sleep(3000);
+            log.info("block2 " + Thread.currentThread().getName() + " end...");
+        }
     }
 }
